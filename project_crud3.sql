0062be53310d2837b58d539473c7fe6c56eae5cb
rizkyamuktii <rizkyamukti@gmail.com>
2026-08-01 08:09:23 +0700

    Update SQL script and modify table structures
    Updated the creation time for the SQL script, modified the address field type in the settings table from varchar to text, and adjusted the AUTO_INCREMENT values for the settings and users tables.

--- project_crud3.sql
+++ project_crud3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Jul 2026 pada 10.02
+-- Waktu pembuatan: 01 Agu 2026 pada 03.08
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -24,6 +24,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `contacs`
+--
+
+CREATE TABLE `contacs` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `resume`
+--
+
+CREATE TABLE `resume` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `year_start` year(4) NOT NULL,
+  `year_end` year(4) NOT NULL,
+  `subtitle` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `settings`
 --
 
@@ -32,12 +65,48 @@ CREATE TABLE `settings` (
   `website_name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `phone` varchar(15) NOT NULL,
-  `address` varchar(200) DEFAULT NULL,
+  `address` text DEFAULT NULL,
   `description` varchar(200) DEFAULT NULL,
   `ig` varchar(50) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `settings`
+--
+
+INSERT INTO `settings` (`id`, `website_name`, `email`, `phone`, `address`, `description`, `ig`, `created_at`, `updated_at`) VALUES
+(1, 'signa.com', 'rizkyamukti@gmail.com', '085782857010', 'tes', '321', 'rizkyamukti@gmail.com', '2026-07-30 02:51:09', '2026-07-30 03:26:52');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sliders`
+--
+
+CREATE TABLE `sliders` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `subtitle` varchar(50) NOT NULL,
+  `description` text DEFAULT NULL,
+  `button1_text` varchar(30) NOT NULL,
+  `button1_link` varchar(50) NOT NULL,
+  `button2_text` varchar(30) NOT NULL,
+  `button2_link` varchar(50) DEFAULT NULL,
+  `image` varchar(100) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `sliders`
+--
+
+INSERT INTO `sliders` (`id`, `title`, `subtitle`, `description`, `button1_text`, `button1_link`, `button2_text`, `button2_link`, `image`, `is_active`, `created_at`, `updated_at`) VALUES
+(1, 'aku cinta prabowo', 'subtitle', 'my bini gw tuhh', 'batak', 'https://mcs.acsl.my.id/', 'timur', 'https://jkd.acsl.my.id/', '6a6c37b58e21e_yuna-itzy-ice-cream-5k-3840x2160-26102.jpg', 0, '2026-07-31 03:03:09', '2026-07-31 05:50:45'),
+(2, 'p wir', 'sunda', ' im', 'batak', 'https://mcs.acsl.my.id/', 'timur', 'https://jkd.acsl.my.id/', '6a6c2da082342_download (1).jpg', 1, '2026-07-31 03:50:18', '2026-07-31 05:07:44');
 
 -- --------------------------------------------------------
 
@@ -57,21 +126,37 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(2, 'Admin', 'admin@gmail.com', '12345678'),
-(4, 'M. Rizkyamukti', 'rizkyamukti@gmail.com', '085782857010'),
-(7, 'gibra parhusip', 'gibran123@gmail.com', '12345678'),
 (8, 'ini ameng', 'creamyhazel@gmail.com', 'hazel123'),
 (9, 'mengskyy', 'creamyhazel@gmail.com', 'mengs123'),
-(10, 'situmorang', 'parhusip@gmail.com', 'bataktimur');
+(13, 'blukutuk', 'papuatimur@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d'),
+(14, 'ambarawa', 'ambarawa@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indeks untuk tabel `contacs`
+--
+ALTER TABLE `contacs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `resume`
+--
+ALTER TABLE `resume`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `settings`
 --
 ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `sliders`
+--
+ALTER TABLE `sliders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -85,16 +170,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `contacs`
+--
+ALTER TABLE `contacs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `resume`
+--
+ALTER TABLE `resume`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `settings`
 --
 ALTER TABLE `settings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `sliders`
+--
+ALTER TABLE `sliders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
